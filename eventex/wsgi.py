641bb300bd02d031e508cd1b80ff6426581319f7
Rodrigo Webler <rwebler@gmail.com>
2013-09-09 18:59:23 +0000

    Consertei o import do dj_static no wsgi.py

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -15,8 +15,6 @@ framework.
 """
 import os
 
-from dj_static import Cling
-
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
@@ -26,6 +24,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())
 
